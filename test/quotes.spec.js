32bf6fad5c1fd7bab0e836885cb46664fdca2b02
Oscar <o@ovl.design>
2019-02-10 00:22:37 +0100

    add tests for closing quotes

--- test/quotes.spec.js
+++ test/quotes.spec.js
@@ -15,28 +15,38 @@ test('Remove ` "`', (t) => {
   t.false(replaced.includes(' "'))
 })
 
-test.skip('Remove `" `', (t) => {
+test('Remove `" `', (t) => {
   t.false(replaced.includes('" '))
 })
 
-test('Quotes - Remove `"` at beginning of string.', (t) => {
+test('Remove `"` at beginning of string.', (t) => {
   t.false(replaced.slice(0, 1) === '"')
 })
 
-test.skip('Quotes - Remove `"` at end of string.', (t) => {
+test('Remove `"` at end of string.', (t) => {
   t.false(replaced.slice(-1) === '"')
 })
 
+test('Remove ` \'`', (t) => {
+  t.false(replaced.includes(` '`))
 })
 
-test('replaceOpeningQuotes does not fix apostrophes', async (t) => {
-  replaced = await Typographizer.replaceOpeningQuotes(exampleEn)
+test('Remove `\',`', (t) => {
+  t.false(replaced.includes(`',`))
+})
 
+test('Do not fix apostrophes', (t) => {
   t.true(replaced.includes(`It's`))
 })
 
-test('replaceOpeningQuotes preserves nested quotes', async (t) => {
-  replaced = await Typographizer.replaceOpeningQuotes(exampleEn)
+test('Do not remove whitespace', (t) => {
+  t.true(replaced.includes('  '))
+})
 
+test('Preserve nested opening quotes', (t) => {
   t.true(replaced.includes('“‘Let'))
+})
+
+test('Preserve nested closing quote', (t) => {
+  t.true(replaced.includes(`’”`))
 })
