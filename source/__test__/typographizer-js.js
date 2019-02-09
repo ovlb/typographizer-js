012f60600fa9bd08a1515d06b8fc3afd3b37b8d8
Oscar <o@ovl.design>
2019-02-09 15:17:47 +0100

    add general tests

---
+++ source/__test__/typographizer-js.js
@@ -0,0 +1,60 @@
+import test from 'ava'
+import TypographizerJS from '../'
+
+import exampleEn from './__samples__/en'
+
+let Typographizer
+let replaced
+
+test.beforeEach(async (t) => {
+  Typographizer = new TypographizerJS()
+  replaced = await Typographizer.typographize(exampleEn)
+})
+
+test('🔧 – Default language is en', (t) => {
+  const { language } = Typographizer.options
+
+  t.is(language, 'en')
+})
+
+test('🔧 – Can set language', (t) => {
+  const TypographizerDE = new TypographizerJS({ language: 'de_DE' })
+  const { language } = TypographizerDE.options
+
+  console.log(TypographizerDE.options)
+
+  t.is(language, 'de_DE')
+})
+
+test('🔧 – Remove ` "`', (t) => {
+  t.false(replaced.includes(' "'))
+})
+
+test('🔧 – Remove `" `', (t) => {
+  t.false(replaced.includes('" '))
+})
+
+test('🔧 - Remove `"` at beginning of string.', (t) => {
+  t.false(replaced.slice(0, 1) === '"')
+})
+
+test('🔧 - Remove `"` at end of string.', (t) => {
+  t.false(replaced.slice(-1) === '"')
+})
+
+// The accent aigu is a diacritical sign and should, in western languages, not stand on its own
+test('🔧 – Remove ´', (t) => {
+  t.false(replaced.includes('´'))
+})
+
+test('🔧 – replaceOpeningQuotes does not fix apostrophes', async (t) => {
+  replaced = await Typographizer.replaceOpeningQuotes(exampleEn)
+
+  t.true(replaced.includes(`It's`))
+})
+
+test('🔧 – replaceOpeningQuotes preserves nested quotes', async (t) => {
+  replaced = await Typographizer.replaceOpeningQuotes(exampleEn)
+
+  t.true(replaced.includes('“‘Let'))
+})
