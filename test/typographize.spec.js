a6d79592b6d15d46cf3f568309b51624c2497e90
Oscar <o@ovl.design>
2019-02-24 00:38:22 +0100

    add test fpr en_GB

--- test/typographize.spec.js
+++ test/typographize.spec.js
@@ -11,3 +11,12 @@ test('en_US', async (t) => {
 
   t.is(replaced, wellFormatted)
 })
+
+test('en_GB', async (t) => {
+  const Typographizer = new TypographizerJS('en_GB')
+  const replaced = await Typographizer.typographize(exampleEn)
+
+  const wellFormatted = `‘Hi’, he said … while running away. Maybe it wasn’t running, maybe it was more like gliding. ‘It’s a beautiful day out in the country.’ He’d been right. Wouldn’t it be just another day full of running away. I looked around and gave a nod to the pink elephant, ‘Hi. “What a time to be alive”, the once alive philosopher said. And died.’ The pink elephant gave me a stern look. ‘“Let it be.”, to quote Paul McCartney. I started responding ‘Well, to quote you “Let it …”’, I stopped. The world was upside down. I began to run and said: ‘Good bye!’`
+
+  t.is(replaced, wellFormatted)
+})
